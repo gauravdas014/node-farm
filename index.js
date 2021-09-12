803886a9f472330cfafb04a9575907b4e2fccc2e
Gaurav Das <gauravdas014@gmail.com>
2021-09-12 13:17:05 +0530

    feat: change server listening call

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, '127.0.0.1', () => {
+server.listen(8000, () => {
   console.log('Listening to requests on port 8000');
 });
